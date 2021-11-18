b988b01fb335bdcbf36b3659d7f6e76066c224fc
suraj9982 <surajsk9982@gmail.com>
2021-11-18 14:47:39 +0530

    [suraj] add : this function checks largest integer

---
+++ GenericProblemsComputation/GenericProblemsComputation/GenericSolution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericProblemsComputation
+{
+    class GenericSolution
+    {
+        public static int FindMax(int firstnumber, int secondnumber, int thirdnumber)
+        {
+            if (firstnumber.CompareTo(secondnumber) > 0 && firstnumber.CompareTo(thirdnumber) > 0)
+            {
+                Console.WriteLine("maximum = " + firstnumber);
+                return firstnumber;
+            }
+            if (secondnumber.CompareTo(firstnumber) > 0 && secondnumber.CompareTo(thirdnumber) > 0)
+            {
+                Console.WriteLine("maximum = " + secondnumber);
+                return secondnumber;
+            }
+            else
+            {
+                Console.WriteLine("maximum = " + thirdnumber);
+                return thirdnumber;
+            }
+        }
+    }
+}
---
+++ GenericProblemsComputation/GenericProblemsComputation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GenericProblemsComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO GENERIC PROBLEMS");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("CHOOSE YOUR OPTION\n1.find maximum integer\n2.exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("enter firstnumber,secondnumber,thirdnumber");
+                        int first = Convert.ToInt32(Console.ReadLine());
+                        int second = Convert.ToInt32(Console.ReadLine());
+                        int third = Convert.ToInt32(Console.ReadLine());
+                        GenericSolution.FindMax(first, second, third);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("your choice should be between 1 to 2");
+                        break;
+                }
+            }
+        }
+    }
+}
